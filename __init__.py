aa74d6d53d98a2ab0de803e9573da483a7c272a8
achraf079 <mester.saber01@gmail.com>
2025-05-09 14:58:17 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -19,7 +19,7 @@ jwt = JWTManager(app)
 def hello_world():
     return render_template('accueil.html')
 
-# Création d'une route qui vérifie l'utilisateur et retour un Jeton JWT si ok.
+# Création d'une route qui vérifie l'utilisateur et retour un Jeton JWT si ok..
 # La fonction create_access_token() est utilisée pour générer un jeton JWT..
 @app.route("/login", methods=["POST"])
 def login():
